b72e64101bc98736166c5fc14ade2695bc7f9a80
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-03 19:48:27 +0100

    Move script

--- assets/python_library/robot.py
+++
@@ -1,5 +0,0 @@
-import dlr
-
-def move_forward():
-    print(overlay.GetClass())
-    print("Moving forward!")
---
+++ assets/robot/python/robot.py
@@ -0,0 +1,8 @@
+# import dlr
+import cs_robot
+
+def move_forward(distance):
+    cs_robot.MoveForward(distance)
+    
+def move_backward(distance):
+    cs_robot.MoveBackward(distance)
