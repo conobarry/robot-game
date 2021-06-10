3ec7d6b8b672d1c01331c4d2eb530862d00d52a6
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-10 17:25:09 +0100

    Add reference to time so that sleep can be used eventually

--- assets/robot/python/test_script.py
+++ assets/robot/python/test_script.py
@@ -1,8 +1,11 @@
 import sys
 import robot
+import time
 
 def test(): 
+    
     robot.move_forward(5)
+    # time.sleep(2)
     robot.move_backward(5)
     # print("Hello there")
     # print >> sys.stderr, "This is an error"
