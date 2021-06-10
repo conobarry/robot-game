297f2030294c30d1ceb6116557d3f3cfb9b1173c
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-10 17:26:18 +0100

    Create TestLevel class So that ScriptEditor can be instantiated with Robot

---
+++ assets/TestLevel.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public class TestLevel : Spatial
+{
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ScriptEditor scriptEditor = GetNode<ScriptEditor>(new NodePath(typeof(ScriptEditor).Name));
+                
+        Robot robot = GetNode<Robot>(new NodePath(typeof(Robot).Name));
+        
+        scriptEditor._Init(robot);
+    }
+
+}
