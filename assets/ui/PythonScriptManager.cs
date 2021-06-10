7888ccfe0186ead8bd509bc3bf5901c87ea80ebf
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-10 17:24:08 +0100

    Add additional methods to run python script

--- assets/ui/PythonScriptManager.cs
+++ assets/ui/PythonScriptManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
+using System.Diagnostics;
 
 public class PythonScriptManager : Node
 {   
@@ -63,6 +64,78 @@ public class PythonScriptManager : Node
         // GD.Print("returned       : ", returned);
         // GD.Print("captured stdout: ", stdout);
         // GD.Print("captured stderr: ", stderr);
+    }
+    
+    public void RunScriptExternal(Robot robot, string script)
+    {
+        Godot.Collections.Array output = new Godot.Collections.Array();
+        int exitCode = OS.Execute("python3", new string[] {"--version"}, false, output);
+        GD.Print("exit code: " + exitCode);
+        GD.Print(output);        
+               
+        
+    }
+    
+    public void RunScriptSharp(Robot robot, string script)
+    {
+        Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/usr/bin/python3",
+                Arguments = "-i",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                CreateNoWindow = true
+            },
+            EnableRaisingEvents = true
+        };
+        
+        process.ErrorDataReceived += Process_OutputDataReceived;
+        process.OutputDataReceived += Process_OutputDataReceived;
+        
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        
+        // StreamReader streamReader = process.StandardOutput;        
+        
+        // StreamWriter streamWriter = process.StandardInput;
+        // streamWriter.WriteLine("print(5)");
+        process.StandardInput.WriteLine("def test():");
+        process.StandardInput.WriteLine("   print(20)");
+        process.StandardInput.WriteLine("");
+        process.StandardInput.WriteLine("test()");
+        process.StandardInput.WriteLine("exit()");
+        
+        // string output = process.StandardOutput.ReadToEnd();
+        // GD.Print(output);
+        
+        // streamWriter.Close();
+        // streamReader.Close();
+        // GD.Print(output); 
+        // process.Kill();        
+        
+        // process.Close();
+        // process.WaitForExit();
+        
+        // Console.Read();               
+        
+        // string command = "-u";
+        // using (Process process = new Process())
+        // {
+        //     process.StartInfo.FileName = "/usr/bin/python3";
+        //     process.StartInfo.UseShellExecute = false;
+        //     process.StartInfo.RedirectStandardInput = true;
+        // }
+    }
+    
+    static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data != null) {GD.Print(e.Data);}
+        // Console.WriteLine(e.Data);
     }
     
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
