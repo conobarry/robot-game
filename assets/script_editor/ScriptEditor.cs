e5f84c78716a30a92158bc37979c799415b59900
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-02 18:51:39 +0100

    Add getting of console

--- assets/script_editor/ScriptEditor.cs
+++ assets/script_editor/ScriptEditor.cs
@@ -5,19 +5,21 @@ public class ScriptEditor : CanvasLayer
     
     public Robot robot;
     private RunButton runButton;
+    private Console console;
     private PythonScriptManager scriptManager;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-    {
-        this.runButton = GetNode<RunButton>(new NodePath("Window/VBoxContainer/InputContainer/RunButton"));
-        this.scriptManager = new PythonScriptManager();        
+    {        
+        this.runButton = GetNode<RunButton>(new NodePath("Window/VBoxContainer/InputContainer/RunButton"));        
+        this.console = GetNode<Console>(new NodePath("Window/VBoxContainer/InputContainer/EditorContainer/Console"));
+        this.scriptManager = new PythonScriptManager();
     }
     
     public ScriptEditor _Init(Robot robot)
-    {        
+    {                
         this.robot = robot;
-        runButton.Connect(nameof(RunButton.OnRunScript), scriptManager, nameof(PythonScriptManager.RunScriptSharp), new Godot.Collections.Array {robot, "test"});
+        this.runButton.Connect(nameof(RunButton.OnRunScript), scriptManager, nameof(PythonScriptManager.RunScript), new Godot.Collections.Array {robot, "test", console});
                 
         return this;
     }
