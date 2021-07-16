fa8b4a7a59983448313b73bf26e2764405947506
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-16 20:01:47 +0100

    Test script just for InitIron

---
+++ assets/script_editor/python/test_script_iron.py
@@ -0,0 +1,15 @@
+import sys
+import robot
+import time
+
+def test(): 
+    
+    robot.move_forward(1)
+    # time.sleep(2)
+    print("Hello there")
+    print >> sys.stderr, "This is an error"
+    print("another line")
+    
+    robot.move_backward(5)
+    die()
+    return 42
