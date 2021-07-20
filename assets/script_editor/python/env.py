151fdd549fb2680e7e2ff2765305cb3de6bfb849
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-20 20:33:39 +0100

    Create seperate function for printing commands

--- assets/script_editor/python/env.py
+++ assets/script_editor/python/env.py
@@ -3,18 +3,19 @@ import sys, builtins
 # Override the python print function and add @print to the front of any output. 
 #   This shows the script manager that this data was output by this script or the user
 def print(*args, sep=' ', end='\n', file=sys.stdout, flush=False):
-    operations = ["@command"]
-    operation = args[0]
-    if (not operation in operations):
-        builtins.print("@print ", end='')    
+    builtins.print("@print:", end='')
     builtins.print(*args, sep=sep, end=end, file=file, flush=flush)
 
+def _print_command(command, *args):
+    builtins.print("@command:", end='')
+    builtins.print(command, *args)
+
 class robot:
-    def command(cmd):
-        print("@command", cmd)
+    def command(command, *args):
+        _print_command(command, *args)
     def move_forward(distance):
-        robot.command(f"move-forward {distance}")    
+        robot.command("move-forward", distance)    
     def move_backward(distance):
-        robot.command(f"move-backward {distance}")
+        robot.command("move-backward", distance)
 
 # Remember to end the file on a blank line
