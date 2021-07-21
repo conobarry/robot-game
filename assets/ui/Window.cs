7ebb920ecf06513e167bac353ae9d0b2ff7842ef
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-21 19:14:12 +0100

    Set up drag

--- assets/ui/Window.cs
+++ assets/ui/Window.cs
@@ -3,10 +3,24 @@ using System;
 
 public class Window : MarginContainer
 {
+
+    private DragArea titlebar;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        // this.titlebar = GetNode<Titlebar>(new NodePath("Panel/VBoxContainer/Titlebar"));
+        this.titlebar = (DragArea)FindNode("DragArea");
+        titlebar.Drag += Drag;
     }
+
     
+    
+    private void Drag(object sender, InputEventMouseMotion args)
+    {
+        RectPosition += args.Relative;
+    }
+
+   
     
 }
