fec5e5bd698f9d099af953278c0e48cbeb283fb1
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-23 19:47:41 +0100

    Disable hardware cursor and replace with sprite

--- levels/test_level/TestLevel.cs
+++ levels/test_level/TestLevel.cs
@@ -3,14 +3,32 @@ using Godot;
 public class TestLevel : Spatial
 {
 	
+	private Sprite cursor;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ScriptEditor scriptEditor = GetNode<ScriptEditor>(new NodePath(typeof(ScriptEditor).Name));
+		ScriptEditor scriptEditor = (ScriptEditor)FindNode("ScriptEditor");
 				
-		Robot robot = GetNode<Robot>(new NodePath(typeof(Robot).Name));
+		Robot robot = (Robot)FindNode("Robot");		
+
+		cursor = (Sprite)FindNode("Cursor");
+
+		Resource plus = ResourceLoader.Load("res://plus.png");
+		Input.SetCustomMouseCursor(plus);
+
+		Input.SetMouseMode(Input.MouseMode.Hidden);
+
+		GD.Print(robot.Name);
 				
 		scriptEditor._Init(robot);
 	}
+
+    public override void _Process(float delta)
+    {
+		cursor.Position = GetViewport().GetMousePosition();
+
+        base._Process(delta);
+    }
 
 }
