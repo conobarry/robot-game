d3979a04dc7fb0c55395167002d0c5a1e5a4b06e
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 07:40:29 +0100

    Remove unused code.

--- assets/ui/HUD.cs
+++ assets/ui/HUD.cs
@@ -2,20 +2,10 @@ using Godot;
 
 public class HUD : CanvasLayer
 {
-    private Vector2 measureStart;
-
-    private Sprite cursor;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        cursor = (Sprite)FindNode("Cursor");
     }
-
-    public void SetCursor(Cursor cursor)
-    {
-        this.cursor = cursor;
-    }
-
 
 }
