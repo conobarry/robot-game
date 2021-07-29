eb21df6ed5f1e77eff548a8cfbcb764f4946254a
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 20:12:04 +0100

    Add default implementations for methods

--- assets/ICodeObject.cs
+++ assets/ICodeObject.cs
@@ -1,13 +1,46 @@
 using Godot;
+using System.Collections.Generic;
 
-interface ICodeObject
+interface ICodeObject<T> where T : Node
 {
-    string CodeName { get; set; }
+    string CodeName { get; }
+
+    List<MeshInstance> HighlightMeshes { get; }
+
+    Material HighlightMaterial { get; }
 
     void Highlight()
     {
-        
+        foreach (MeshInstance mesh in HighlightMeshes)
+        {
+            mesh.GetActiveMaterial(0).NextPass = HighlightMaterial;
+        }
     }
 
-    void RemoveHighlight();
+    void RemoveHighlight()
+    {
+        foreach (MeshInstance mesh in HighlightMeshes)
+        {
+            mesh.GetActiveMaterial(0).NextPass = null;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        Node node = (Node) this;
+        Cursor cursor = (Cursor) node.GetTree().GetNodesInGroup("cursor")[0];
+        cursor.TooltipUpper.Text = CodeName;
+
+        Highlight();
+    }
+
+    void OnMouseExit()
+    {
+        Node node = (Node) this;
+        Cursor cursor = (Cursor) node.GetTree().GetCursor();
+        cursor.TooltipUpper.Text = "";
+
+        RemoveHighlight();
+    }
+
 }
