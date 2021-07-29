ea16d4041c31c15615e19d74618c6052ddae0991
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 07:44:44 +0100

    Move code to superclass

--- assets/ToolCamera.cs
+++ assets/ToolCamera.cs
@@ -1,30 +1,12 @@
 using Godot;
 
-public class ToolCamera : Godot.Camera
+public class ToolCamera : Camera
 {
-    
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
-
     public override void _Process(float delta)
     {
+        // Make this camera follow the main level camera
         Transform = this.GetTree().Root.GetCamera().Transform;
 
         base._Process(delta);
     }
-
-    public Vector3? ViewportPointToGround(Vector2 viewportPoint)
-    {
-        Plane groundPlane = new Plane(0, 1, 0, 0);
-        Vector3 origin = ProjectRayOrigin(viewportPoint);
-        Vector3 normal = ProjectRayNormal(viewportPoint);
-        return groundPlane.IntersectRay(origin, normal);
-    }
-
-    public Vector2 GroundPointToViewport(Vector3 groundPoint)
-    {
-        return UnprojectPosition(groundPoint);
-    }    
 }
