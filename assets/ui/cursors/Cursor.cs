54029ec680e9cec24a93b38fed676f8b8b4ee53a
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 07:36:37 +0100

    Improve tooltip usage and use switch to select cursor

--- assets/ui/cursors/Cursor.cs
+++ assets/ui/cursors/Cursor.cs
@@ -2,19 +2,26 @@ using Godot;
 
 public class Cursor : Sprite
 {
-    // public Vector2 Offset { get; set; }
+    public Label TooltipUpper { get; private set; }
 
-    // public Texture Texture { get; set; }
+    public Label TooltipLower { get; private set; }
 
-    public static Cursor Arrow;
+    private Cursor defaultCursor;
 
-    public static Cursor Pointer;
+    private Cursor arrow;
 
-    public static Cursor IBeam;
+    private Cursor pointer;
+
+    private Cursor iBeam;
     
-    public static Cursor Cross;
+    private Cursor cross;
 
-    public static Cursor Ruler;
+    private Cursor ruler;
+
+    public Cursor()
+    {
+
+    }
 
     private Cursor(Texture texture, Vector2 offset)
     {
@@ -24,23 +31,79 @@ public class Cursor : Sprite
 
     public override void _Ready()
     {
+        TooltipUpper = (Label) FindNode("UpperTooltip");
+        TooltipLower = (Label) FindNode("LowerTooltip");
+
+        // Hide os cursor
+		Input.SetMouseMode(Input.MouseMode.Hidden);
+
         Texture arrowTexture = GD.Load<Texture>("res://assets/ui/cursors/arrow.png");
-        Arrow = new Cursor(arrowTexture, new Vector2(12, 12));
+        arrow = new Cursor(arrowTexture, new Vector2(12, 12));
 
         Texture pointerTexture = GD.Load<Texture>("res://assets/ui/cursors/pointer.png");
-        Pointer = new Cursor(pointerTexture, new Vector2(6, 12));
+        pointer = new Cursor(pointerTexture, new Vector2(6, 12));
 
         Texture rulerTexture = GD.Load<Texture>("res://assets/ui/cursors/ruler.png");
-        Ruler = new Cursor(rulerTexture, new Vector2(7, 11));
+        ruler = new Cursor(rulerTexture, new Vector2(7, 11));
+
+        Texture beamTexture = GD.Load<Texture>("res://assets/ui/cursors/beam.png");
+        iBeam = new Cursor(beamTexture, new Vector2(1, 1));
+
+        Texture crossTexture = GD.Load<Texture>("res://assets/ui/cursors/cross.png");
+        cross = new Cursor(crossTexture, new Vector2(0, 0));        
+
+        defaultCursor = arrow;
+    }
+
+    public override void _Process(float delta)
+    {
+        Position = GetViewport().GetMousePosition();
+
+        base._Process(delta);
+    }
+
+    public void SetCursor(CursorType cursorType)
+    {
+        Cursor newCursor = defaultCursor;
+        
+        switch (cursorType)
+        {
+            case CursorType.Default:
+                newCursor = defaultCursor;
+                break;
+            case CursorType.Arrow:
+                newCursor = arrow;
+                break;
+            case CursorType.Pointer:
+                newCursor = pointer;
+                break;
+            case CursorType.IBeam:
+                newCursor = iBeam;
+                break;
+            case CursorType.Cross:
+                newCursor = cross;
+                break;
+            case CursorType.Ruler:
+                newCursor = ruler;
+                break;
+        }
+
+        this.Texture = newCursor.Texture;
+        this.Offset = newCursor.Offset;
+    }
+
+    public void SetDefault()
+    {
+        SetCursor(CursorType.Default);
     }
 }
 
-// public enum CursorType
-// {
-//     Default,
-//     Arrow,
-//     Pointer,
-//     IBeam,
-//     Cross,
-//     Ruler        
-// }
+public enum CursorType
+{
+    Default,
+    Arrow,
+    Pointer,
+    IBeam,
+    Cross,
+    Ruler        
+}
