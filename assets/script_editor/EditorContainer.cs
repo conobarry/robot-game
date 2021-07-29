272a73e5ac5fb844295720ba3af3c9e40d781873
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 20:15:45 +0100

    Create file

---
+++ assets/script_editor/EditorContainer.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public class EditorContainer : VSplitContainer
+{
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+            
+    }
+
+
+
+}
