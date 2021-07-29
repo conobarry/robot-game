be54df33abb17c400fe4aac9cf9ebcbbc75ab650
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 20:12:33 +0100

    Change cursor on hover

--- assets/ui/RunButton.cs
+++ assets/ui/RunButton.cs
@@ -2,13 +2,26 @@ using System;
 using Godot;
 
 public class RunButton : Button
-{        
-    
-    // [Signal]
-    // public delegate void OnRunScript();
+{    
 
     public event EventHandler ButtonPressed;
-    
+
+    public override void _Ready()
+    {
+        Connect("mouse_entered", this, nameof(OnMouseEnter));
+        Connect("mouse_exited", this, nameof(OnMouseExit));
+    }
+
+    private void OnMouseEnter()
+    {
+        GetTree().GetCursor().SetCursor(CursorType.Pointer);
+    }
+
+    private void OnMouseExit()
+    {
+        GetTree().GetCursor().SetCursor(CursorType.Default);
+    }
+
     public virtual void OnButtonPressed()
     {
         // EmitSignal(nameof(OnRunScript));
