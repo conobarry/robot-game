0093b85f15a132f51c883093e107ac340b9447e3
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 07:37:47 +0100

    Add custom camera implementation

---
+++ assets/Camera.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public class Camera : Godot.Camera
+{
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public Vector3? ViewportPointToGround(Vector2 viewportPoint)
+    {
+        Plane groundPlane = new Plane(0, 1, 0, 0);
+        Vector3 origin = ProjectRayOrigin(viewportPoint);
+        Vector3 normal = ProjectRayNormal(viewportPoint);
+        return groundPlane.IntersectRay(origin, normal);
+    }
+
+    public Vector2 GroundPointToViewport(Vector3 groundPoint)
+    {
+        return UnprojectPosition(groundPoint);
+    }
+
+}
