227aa46cf01816b556f90955de9a8faade817145
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-29 07:39:48 +0100

    Add ability to change cursor type while measuring

--- levels/test_level/MeasureLine.cs
+++ levels/test_level/MeasureLine.cs
@@ -4,47 +4,57 @@ using Godot;
 public class MeasureLine : ImmediateGeometry
 {
 
-    private Vector3 measureStart;
+    private Vector3 measureStart = Vector3.Zero;
 
     private ToolCamera camera;
 
+    private Cursor cursor;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         camera = (ToolCamera) GetViewport().GetCamera();
+    }
 
-        // GD.Print(GetParent());
-        // camera = (Camera) GetParent<Camera>();
+    public void _Init(Cursor cursor)
+    {
+        this.cursor = cursor;
     }
 
     public override void _Process(float delta)
     {
         Vector2 mousePos = GetViewport().GetMousePosition();
         Vector3 mousePos3D = camera.ViewportPointToGround(mousePos).GetValueOrDefault();
+        float length = (mousePos3D - measureStart).Length();
 
         if (mousePos3D != default(Vector3))
         {
             if (Input.IsActionJustPressed("tools_measure"))
             {
                 measureStart = mousePos3D;
+                UpdateCursor(length);
             }
             if (Input.IsActionPressed("tools_measure"))
             {
                 Clear();
-                // DrawLine(measureStart, mousePos3D, Vector3.Up, 0.2f);
-                // DrawSector(mousePos3D, 1, Mathf.Pi * 2, new Vector3(0, 0, 1), 20, Vector3.Up);
-                DrawMeasureLine(measureStart, mousePos3D, Vector3.Up, 0.1f);
-                
+                DrawMeasureLine(measureStart, mousePos3D, Vector3.Up, 0.1f);   
+                UpdateCursor(length);             
             }
             if (Input.IsActionJustReleased("tools_measure"))
             {
                 Clear();
+                cursor.SetDefault();
+                cursor.TooltipLower.Text = "";
             }
-        }       
-        
+        }
 
         base._Process(delta);
+    }
+
+    private void UpdateCursor(float length)
+    {
+        cursor.SetCursor(CursorType.Ruler);
+        cursor.TooltipLower.Text = $"{length.ToString("F2")}m";
     }
 
     private void DrawLine(Vector3 start, Vector3 end, Vector3 normal, float thickness)
@@ -65,14 +75,7 @@ public class MeasureLine : ImmediateGeometry
 
         End();
         
-    }
-
-    private void DrawMeasureLine(Vector3 start, Vector3 end, Vector3 normal, float thickness)
-    {
-        DrawLine(start, end, normal, thickness);
-        DrawSector(start, thickness, Mathf.Pi, start - end, 8, normal);
-        DrawSector(end, thickness, Mathf.Pi, end - start, 8, normal);
-    }
+    }    
 
     private void DrawSector(Vector3 center, float radius, float angle, Vector3 direction, int segments, Vector3 normal)
     {
@@ -81,11 +84,6 @@ public class MeasureLine : ImmediateGeometry
         direction = direction.IsNormalized() ? direction : direction.Normalized();
 
         float angleStep = angle / segments;
-
-        // 1 = 0.5
-        // 2 = 1
-        // 3 = 1.5
-        // 4 = 2
 
         Vector3 directionStep = direction.Rotated(normal, (angleStep * (segments / 2)));
 
@@ -98,5 +96,12 @@ public class MeasureLine : ImmediateGeometry
         }
 
         End();
+    }
+    
+    private void DrawMeasureLine(Vector3 start, Vector3 end, Vector3 normal, float thickness)
+    {
+        DrawLine(start, end, normal, thickness);
+        DrawSector(start, thickness, Mathf.Pi, start - end, 8, normal);
+        DrawSector(end, thickness, Mathf.Pi, end - start, 8, normal);
     }
 }
