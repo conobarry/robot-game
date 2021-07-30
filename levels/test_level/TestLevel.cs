4e128b0e36fc3ffab277ad0bdc3f4de152fb2d27
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-30 22:01:37 +0100

    Connect to pads and progress bar

--- levels/test_level/TestLevel.cs
+++ levels/test_level/TestLevel.cs
@@ -3,6 +3,8 @@ using Godot;
 public class TestLevel : Spatial
 {
 	PauseMenu pauseMenu;
+	
+	ProgressMeter progressMeter;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()	
@@ -10,20 +12,28 @@ public class TestLevel : Spatial
 		Robot robot = (Robot) FindNode("Robot");
 
 		ScriptEditor scriptEditor = (ScriptEditor) FindNode("ScriptEditor");
-		scriptEditor._Init(robot);		
+		scriptEditor._Init(robot);
 
 		// Set new cursor
 		Cursor cursor = (Cursor) FindNode("Cursor");
 		cursor.SetCursor(CursorType.Arrow);
 		
 		MainCamera camera = (MainCamera) FindNode("MainCamera");
-		camera._Init(cursor);
+		camera._Init(cursor, robot);
 
 		ToolView toolView = (ToolView) FindNode("ToolView");
 		toolView._Init(cursor);
 
 		pauseMenu = (PauseMenu) FindNode("PauseMenu");
-		// pauseMenu.Visible = false;
+		
+		Godot.Collections.Array pads = GetTree().GetNodesInGroup("pad");
+		foreach (Pad pad in pads)
+		{
+			pad.Connect("triggered", this, nameof(OnPadTriggered));
+		}
+
+		progressMeter = (ProgressMeter) FindNode("ProgressMeter");
+		progressMeter._Init("Push the boxes!", 0, pads.Count);
 	}
 
 	public override void _Process(float delta)
@@ -33,6 +43,11 @@ public class TestLevel : Spatial
 			pauseMenu.Popup_();
 			GetTree().Paused = true;
 		}
+	}
+
+	private void OnPadTriggered()
+	{
+		progressMeter.CurrentValue += 1;
 	}
 
 }
