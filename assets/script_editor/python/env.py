49d56e8034d8f146b5c3754feaba530f3ae3c5ca
conobarry <52550366+conobarry@users.noreply.github.com>
2021-09-10 14:58:04 +0100

    Add comments

--- assets/script_editor/python/env.py
+++ assets/script_editor/python/env.py
@@ -1,15 +1,17 @@
 import sys, builtins
 
-# Override the python print function and add @print to the front of any output. 
-#   This shows the script manager that this data was output by this script or the user
+# Override the python print function and add @print to the front of any output.
 def print(*args, sep=' ', end='\n', file=sys.stdout, flush=False):
     builtins.print("@print:", end='')
     builtins.print(*args, sep=sep, end=end, file=file, flush=flush)
 
+# Prepends @command to *args and prints to STDOUT using the builtin print function.
 def _print_command(command, *args):
     builtins.print("@command:", end='')
     builtins.print(command, *args)
 
+# Defines methods and properties of the robot that the player can call.
+#   Calling these methods prints that command to STDOUT.
 class robot:
     def command(command, *args):
         _print_command(command, *args)
